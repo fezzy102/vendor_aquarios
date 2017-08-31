a32d5fe17474938f9b73f7e9ac4bb4da9c9cf58a
viperusgit <viperusgit@gmail.com>
2017-08-30 19:56:21 -0500

    Add in dialer_experience.xml to fix google phone bitching at us
    Signed-off-by: calebcabob <calphonic@gmail.com>

--- products/aosp_fixes.mk
+++ products/aosp_fixes.mk
@@ -37,6 +37,10 @@ PRODUCT_COPY_FILES += \
     vendor/aquarios/prebuilt/lib64/libjni_latinime.so:system/lib64/libjni_latinime.so \
     vendor/aquarios/prebuilt/lib64/libjni_latinimegoogle.so:system/lib64/libjni_latinimegoogle.so
 
+# Fix Dialer
+PRODUCT_COPY_FILES +=  \
+    vendor/aquarios/prebuilt/etc/sysconfig/dialer_experience.xml:system/etc/sysconfig/dialer_experience.xml
+
 # Clean up packages cache to avoid wrong strings and resources
 PRODUCT_COPY_FILES += \
     vendor/aquarios/prebuilt/bin/clean_cache.sh:system/bin/clean_cache.sh
