943998c314596242e4b06c85f51d99b5289735b2
calebcabob <calphonic@gmail.com>
2017-11-07 13:30:23 -0500

    Products: Update device fingerprints for November security patch
    *Used SuperR's kitchen

--- products/sailfish.mk
+++ products/sailfish.mk
@@ -28,5 +28,5 @@ PRODUCT_MANUFACTURER := Google
 # Device Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=sailfish \
-    BUILD_FINGERPRINT=google/sailfish/sailfish:8.0.0/OPR3.170623.008/4294783:user/release-keys \
-    PRIVATE_BUILD_DESC="sailfish-user 8.0.0 OPR3.170623.008 4294783 release-keys"
+    BUILD_FINGERPRINT=google/sailfish/sailfish:8.0.0/OPR3.170623.013/4397526:user/release-keys \
+    PRIVATE_BUILD_DESC="sailfish-user 8.0.0 OPR3.170623.013 4397526 release-keys"
