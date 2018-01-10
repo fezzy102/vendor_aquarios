VorVanille/android_vendor_vanilla

Add in dialer_experience.xml to fix google phone bitching at us

configs/aosp_fixes.mk
@@ -42,3 +42,7 @@ PRODUCT_COPY_FILES +=  \
     vendor/vanilla/prebuilt/media/LMspeed_508.emd:system/vendor/media/LMspeed_508.emd \
     vendor/vanilla/prebuilt/media/PFFprec_600.emd:system/vendor/media/PFFprec_600.emd
 endif
+
+# Fix Dialer
+PRODUCT_COPY_FILES +=  \
+    vendor/vanilla/prebuilt/etc/sysconfig/dialer_experience.xml:system/etc/sysconfig/dialer_experience.xml 

fezzy102/vendor_aquarios

Signed-off-by: calebcabob <calphonic@gmail.com>

configs/aosp_fixes.mk
@@ -42,6 +42,10 @@ PRODUCT_COPY_FILES += \
     vendor/aquarios/prebuilt/lib64/libjni_latinimegoogle.so:system/lib64/libjni_latinimegoogle.so
 endif
 
+# Fix Dialer
+PRODUCT_COPY_FILES +=  \
+    vendor/aquarios/prebuilt/etc/sysconfig/dialer_experience.xml:system/etc/sysconfig/dialer_experience.xml
+
 # Clean up packages cache to avoid wrong strings and resources
 PRODUCT_COPY_FILES += \
     vendor/aquarios/prebuilt/bin/clean_cache.sh:system/bin/clean_cache.sh