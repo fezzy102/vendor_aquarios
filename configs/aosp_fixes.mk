spezi77/abc_vendor_nexus

Fix AOSP gesture typing

configs/aosp_fixes.mk
@@ -28,10 +28,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Proprietary latinime libs needed for Keyboard swyping
 ifneq ($(filter shamu,$(TARGET_PRODUCT)),)
 PRODUCT_COPY_FILES += \
-    vendor/nexus/prebuilt/lib/libjni_latinime.so:system/lib/libjni_latinime.so
+    vendor/nexus/prebuilt/lib/libjni_latinime.so:system/lib/libjni_latinime.so \
+    vendor/nexus/prebuilt/lib/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so
 else
 PRODUCT_COPY_FILES += \
-    vendor/nexus/prebuilt/lib64/libjni_latinime.so:system/lib64/libjni_latinime.so
+    vendor/nexus/prebuilt/lib64/libjni_latinime.so:system/lib64/libjni_latinime.so \
+    vendor/nexus/prebuilt/lib64/libjni_latinimegoogle.so:system/lib64/libjni_latinimegoogle.so
 endif
 
 # Camera Effects for devices without a vendor partition

fezzy102/vendor_aquarios

Signed-off-by: calebcabob <calphonic@gmail.com>

configs/aosp_fixes.mk
@@ -34,8 +34,10 @@ $(call inherit-product-if-exists, frameworks/base/data/sounds/GoogleAudio.mk)
 # Proprietary latinime libs needed for Keyboard swyping
 ifneq ($(filter shamu,$(TARGET_PRODUCT)),)
 PRODUCT_COPY_FILES += \
-    vendor/aquarios/prebuilt/lib/libjni_latinime.so:system/lib/libjni_latinime.so
+    vendor/aquarios/prebuilt/lib/libjni_latinime.so:system/lib/libjni_latinime.so \
+    vendor/aquarios/prebuilt/lib/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so
 else
 PRODUCT_COPY_FILES += \
-    vendor/aquarios/prebuilt/lib64/libjni_latinime.so:system/lib64/libjni_latinime.so
+    vendor/aquarios/prebuilt/lib64/libjni_latinime.so:system/lib64/libjni_latinime.so \
+    vendor/aquarios/prebuilt/lib64/libjni_latinimegoogle.so:system/lib64/libjni_latinimegoogle.so
 endif