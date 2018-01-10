spezi77/abc_vendor_nexus

Add a prebuilt busybox fstrim bin (for Tasker or terminal)
E.g. You can create a weekly scheduled task in Tasker
to trim /system/ and /data partitions:

su
fstrim -v /system
fstrim -v /data

Bin built with NDK tc.

configs/system_additions.mk
@@ -27,6 +27,10 @@ PRODUCT_COPY_FILES += \
     vendor/nexus/prebuilt/bin/backuptool.functions:install/bin/backuptool.functions \
     vendor/nexus/prebuilt/bin/50-base.sh:system/addon.d/50-base.sh \
 
+# Prebuilt busybox fstrim command
+PRODUCT_COPY_FILES += \
+    vendor/nexus/prebuilt/bin/fstrim:system/bin/fstrim
+
 # Security Enhanced Linux
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.selinux=1

fezzy102/vendor_aquarios

Signed-off-by: calebcabob <calphonic@gmail.com>

configs/system_additions.mk
@@ -23,6 +23,10 @@ PRODUCT_COPY_FILES += \
     vendor/aquarios/prebuilt/bin/backuptool.functions:install/bin/backuptool.functions \
     vendor/aquarios/prebuilt/bin/backuptool.sh:install/bin/backuptool.sh 
 
+# Prebuilt busybox fstrim command
+PRODUCT_COPY_FILES += \
+    vendor/aquarios/prebuilt/bin/fstrim:system/bin/fstrim
+
 # Disable Rescue Party
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.disable_rescue=true